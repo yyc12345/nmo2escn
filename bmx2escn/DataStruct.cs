5f628bb2c8471f79b7017d431fba73063c91f58b
yyc12345 <yyc12321@outlook.com>
2022-03-17 16:04:51 +0800

    add texture and material export in EscnWriter

---
+++ bmx2escn/DataStruct.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bmx2escn.DataStruct {
+
+    public class BMXColor {
+        public float R;
+        public float G;
+        public float B;
+    }
+
+    public class BMXPoint3D {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+    public class BMXPoint2D {
+        public float U;
+        public float V;
+    }
+
+    public class BMXFace {
+        public UInt32 vertex_1;
+        public UInt32 texture_1;
+        public UInt32 normal_1;
+        public UInt32 vertex_2;
+        public UInt32 texture_2;
+        public UInt32 normal_2;
+        public UInt32 vertex_3;
+        public UInt32 texture_3;
+        public UInt32 normal_3;
+
+        public bool use_material;
+        public UInt32 material_index;
+    }
+
+
+    public class ChunkTexture {
+        public UInt32 INDEX;
+        public string NAME;
+
+        public string filename;
+        public bool is_external;
+    }
+
+    public class ChunkMaterial {
+        public UInt32 INDEX;
+        public string NAME;
+
+        public BMXColor ambient;
+        public BMXColor diffuse;
+        public BMXColor specular;
+        public BMXColor emissive;
+        public float specular_power;
+        public bool use_texture;
+        public UInt32 map_kd;
+    }
+
+    public class ChunkMesh {
+        public UInt32 INDEX;
+        public string NAME;
+        
+        public List<BMXPoint3D> v_list;
+        public List<BMXPoint2D> vt_list;
+        public List<BMXPoint3D> vn_list;
+        public List<BMXFace> face_list;
+    }
+
+}
--- bmx2escn/EscnWriter.cs
+++ bmx2escn/EscnWriter.cs
@@ -1,13 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace bmx2escn {
-    public class EscnWriter {
-        public EscnWriter(string filepath) {
 
+    public class EscnFace {
+        public bool use_material = false;
+        public UInt32 material_index = 0;
+
+        public List<DataStruct.BMXPoint3D> vertexs = new List<DataStruct.BMXPoint3D>();
+        public List<DataStruct.BMXPoint3D> normals = new List<DataStruct.BMXPoint3D>();
+        public List<DataStruct.BMXPoint2D> uvs = new List<DataStruct.BMXPoint2D>();
+        public List<UInt32> indices = new List<UInt32>();
+
+        UInt32 vertice_count = 0;
+        Dictionary<int, UInt32> vertice_index_map = new Dictionary<int, UInt32>();
+
+        public void Add(DataStruct.BMXPoint3D vertex, DataStruct.BMXPoint3D normal, DataStruct.BMXPoint2D uv) {
+            var hash = ComputVerticeHash(vertex, normal, uv);
+            UInt32 index;
+
+            if (!vertice_index_map.TryGetValue(hash, out index)) {
+                // new vertice
+                index = vertice_count;
+                vertice_count++;
+
+                vertice_index_map.Add(hash, index);
+                this.vertexs.Add(vertex);
+                this.normals.Add(normal);
+                this.uvs.Add(uv);
+
+            }
+            indices.Add(index);
         }
 
+        private int ComputVerticeHash(DataStruct.BMXPoint3D vertex, DataStruct.BMXPoint3D normal, DataStruct.BMXPoint2D uv) {
+            return HashCode.Combine(vertex.X, vertex.Y, vertex.Z, normal.X, normal.Y, normal.Z, uv.U, uv.V);
+        }
+
+    }
+
+    public class EscnWriter : IDisposable {
+
+        static UInt32 INVALID_ID = 0;
+
+        public EscnWriter(string filepath, string internalTexturePath, UInt32 textureCount, UInt32 mtlCount, UInt32 meshCount) {
+            mExtResCounter = mSubResCounter = 1;
+            mTextureMap = new UInt32[textureCount];
+            mMtlMap = new UInt32[mtlCount];
+            mMeshMap = new UInt32[meshCount];
+            Array.Fill(mTextureMap, INVALID_ID);
+            Array.Fill(mMtlMap, INVALID_ID);
+            Array.Fill(mMeshMap, INVALID_ID);
+
+            mInternalTexturePath = internalTexturePath;
+
+            // open file and write head
+            mfs = new StreamWriter(filepath, false, Encoding.UTF8);
+            mfs.WriteLine("[gd_scene load_steps=1 format=2]");
+        }
+
+        StreamWriter mfs;
+        string mInternalTexturePath;
+        UInt32 mExtResCounter, mSubResCounter;
+        UInt32[] mTextureMap, mMtlMap, mMeshMap;
+
+        public void Dispose() {
+            mfs.Close();
+            mfs.Dispose();
+        }
+
+        private UInt32 AllocExtResId() => mExtResCounter++;
+        private UInt32 AllocSubResId() => mSubResCounter++;
+
+        public void WriteTexture(DataStruct.ChunkTexture texture) {
+            UInt32 id = AllocExtResId();
+            mTextureMap[texture.INDEX] = id;
+
+            string path;
+            if (texture.is_external) path = mInternalTexturePath + texture;
+            else path = texture.filename;
+
+            mfs.WriteLine($"[ext_resource id={id} path=\"{path}\" type=\"Texture\"]");
+        }
+
+        public void WriteMaterial(DataStruct.ChunkMaterial material) {
+            UInt32 id = AllocSubResId();
+            mMtlMap[material.INDEX] = id;
+
+            mfs.WriteLine($"[sub_resource id={id} type=\"SpatialMaterial\"]");
+            mfs.WriteLine($"resource_name = \"{material.NAME}\"");
+            mfs.WriteLine($"albedo_color = Color({material.diffuse.R}, {material.diffuse.G}, {material.diffuse.B}, 1.0)");
+            mfs.WriteLine($"metallic = {(material.ambient.R + material.ambient.G + material.ambient.B) / 3.0f}");
+            mfs.WriteLine($"metallic_specular = {(material.specular.R + material.specular.G + material.specular.B) / 3.0f}");
+            mfs.WriteLine($"metallic_specular = {(material.specular.R + material.specular.G + material.specular.B) / 3.0f}");
+            
+            if (material.use_texture) {
+                if (mTextureMap[material.map_kd] == INVALID_ID)
+                    throw new Exception($"Chunk lost: Texture{material.map_kd}");
+
+                mfs.WriteLine($"albedo_texture = ExtResource({mTextureMap[material.map_kd]})");
+            }
+        }
+
+        public void WriteMesh(DataStruct.ChunkMesh mesh) {
+            UInt32 id = AllocSubResId();
+            mMeshMap[mesh.INDEX] = id;
+
+            // todo: split mesh by material
+
+
+            mfs.WriteLine($"[sub_resource id={id} type=\"ArrayMesh\"]");
+            mfs.WriteLine($"resource_name = \"{mesh.NAME}\"");
+
+
+        }
 
     }
 }
